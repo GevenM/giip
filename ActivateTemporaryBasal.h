179b476c043ff2421044d32261cf240371d507e4
ProsciaN <nicholas_proscia@hotmail.com>
2014-07-17 18:13:48 -0400

    Created Temporary Basal Files

---
+++ ActivateTemporaryBasal.h
@@ -0,0 +1,10 @@
+#ifndef CREATETEMPORARYBASAL_H
+#define CREATETEMPORARYBASAL_H
+
+#include "Shared.h"
+#include "MonitoredVariables.h"
+
+extern void ActivateTemporaryBasal();
+extern bool TemporaryBasalActivationCompleted();
+
+#endif
