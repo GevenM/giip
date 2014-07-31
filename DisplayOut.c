771b64b54057bf98559576c1669629a13eab790d
ProsciaN <nicholas_proscia@hotmail.com>
2014-07-31 12:23:53 -0400

    updated displayout with datetime removals and parameters

--- DisplayOut.c
+++ DisplayOut.c
@@ -79,11 +79,6 @@ void InputProfileToBasalProfile(y_basal *basProf);
 
 void PrintRemoveBolusPreset_Idle();
 
-
-
-void PrintSettings_DateTime();
-void PrintSettings_DateTime_NotAllowed();
-
 void PrintCreateReminder_Idle();
 void PrintCreateReminder_Confirm();
 void PrintCreateReminder_Invalid();
@@ -201,8 +196,8 @@ void PrintScreen(){
 	case RemoveReminder_Idle:PrintRemoveReminder_Idle(); break;
 	case RemoveReminder_Confirm: PrintRemoveReminder_Confirm();break;
 
-	case Settings_DateTime: PrintSettings_DateTime(); break;
-	case Settings_DateTime_NotAllowed: PrintSettings_DateTime_NotAllowed(); break;
+	case Settings_DateTime: PrintSettings_DateTime(&g_sContext, p_calendar); break;
+	case Settings_DateTime_NotAllowed: PrintSettings_DateTime_NotAllowed(&g_sContext); break;
 
 
 	default: PrintError(); break;
@@ -1860,122 +1855,6 @@ void PrintRemoveBolusPreset_Idle(){
 	LoadLeftButton( "CANC" );
 	LoadMiddleButton( "SEL" );
 	GrFlush(&g_sContext);
-}
-
-
-
-
-
-
-
-void  PrintSettings_DateTime(){
-	// Clear previous entries from screen
-	GrContextForegroundSet(&g_sContext, ClrWhite);
-	GrRectFill(&g_sContext, &myRectangleScreen);
-	GrContextForegroundSet(&g_sContext, ClrBlack);
-
-	int digits = 0, domDigits = 0;
-	char buffer[10] = "";
-	char outString[32] = "";
-
-	/* DRAW TIME */
-	strcpy( outString, "Time: " );
-
-	// Get Hours
-	digits = UnsignedInt_To_ASCII( p_calendar.Hours >> 4, buffer ); // Read upper half of byte by bitshifting four places
-	strncat(outString, buffer, digits);
-	digits = UnsignedInt_To_ASCII( p_calendar.Hours & 0xF, buffer ); // Read lower half of byte by overwriting the upper half with 0s
-	strncat(outString, buffer, digits);
-	strncat(outString, ":", 1);
-
-	// Get Minutes
-	digits = UnsignedInt_To_ASCII( p_calendar.Minutes >> 4, buffer );
-	strncat(outString, buffer, digits);
-	digits = UnsignedInt_To_ASCII( p_calendar.Minutes & 0xF, buffer );
-	strncat(outString, buffer, digits);
-	strncat(outString, ":", 1);
-
-	// Get Seconds
-	digits = UnsignedInt_To_ASCII( p_calendar.Seconds >> 4, buffer );
-	strncat(outString, buffer, digits);
-	digits = UnsignedInt_To_ASCII( p_calendar.Seconds & 0xF, buffer );
-	strncat(outString, buffer, digits);
-
-	GrStringDraw(&g_sContext, outString, AUTO_STRING_LENGTH, 5 , 40, TRANSPARENT_TEXT);
-
-
-	/* DRAW DATE */
-	strcpy( outString, "Dt: " );
-
-	// Get Day of month
-	domDigits = UnsignedInt_To_ASCII( BCDtoInt( p_calendar.DayOfMonth ), buffer ); // Read upper half of byte by bitshifting four places
-	strncat(outString, buffer, domDigits);
-
-	// Get Month
-	switch ( BCDtoInt( p_calendar.Month ) ){
-	case 1: strncat(outString, " Jan ", 5); break;
-	case 2: strncat(outString, " Feb ", 5); break;
-	case 3: strncat(outString, " Mar ", 5); break;
-	case 4: strncat(outString, " Apr ", 5); break;
-	case 5: strncat(outString, " May ", 5); break;
-	case 6: strncat(outString, " Jun ", 5); break;
-	case 7: strncat(outString, " Jul ", 5); break;
-	case 8: strncat(outString, " Aug ", 5); break;
-	case 9: strncat(outString, " Sep ", 5); break;
-	case 10: strncat(outString, " Oct ", 5); break;
-	case 11:strncat(outString, " Nov ", 5); break;
-	case 12:strncat(outString, " Dec ", 5); break;
-	default: break;
-	}
-
-	// Get Year
-	digits = UnsignedInt_To_ASCII( p_calendar.Year >> 12, buffer );
-	strncat(outString, buffer, digits);
-	digits = UnsignedInt_To_ASCII( p_calendar.Year >> 8 & 0xF, buffer );
-	strncat(outString, buffer, digits);
-	digits = UnsignedInt_To_ASCII( p_calendar.Year >> 4 & 0xF, buffer );
-	strncat(outString, buffer, digits);
-	digits = UnsignedInt_To_ASCII( p_calendar.Year & 0xF, buffer );
-	strncat(outString, buffer, digits);
-
-
-
-	/* Draw Cursor */
-	int cursorY, cursorX, cursorW;
-	switch ( p_calendarIndex ){
-	case SEC: 		cursorX = 77; 					cursorY = 48; cursorW = 10;					break;
-	case MIN: 		cursorX = 59;				 	cursorY = 48; cursorW = 10;					break;
-	case HR: 		cursorX = 41; 					cursorY = 48; cursorW = 10;					break;
-	case MON:		cursorX = 35 + (domDigits * 6); cursorY = 63; cursorW = 16;					break;
-	case DOM:		cursorX = 29; 					cursorY = 63; cursorW = (domDigits * 5);	break;
-	case YEAR_1:	cursorX = 59 + (domDigits * 6); cursorY = 63; cursorW = 10;					break;
-	case YEAR_2:	cursorX = 71 + (domDigits * 6); cursorY = 63; cursorW = 10;					break;
-	}
-
-	GrLineDrawH(&g_sContext, cursorX, cursorX+cursorW, cursorY);
-
-
-	GrStringDraw(&g_sContext, outString, AUTO_STRING_LENGTH, 5 , 55, TRANSPARENT_TEXT);
-
-	GrStringDrawCentered(&g_sContext, "Edit Calendar", AUTO_STRING_LENGTH, 46 , 27, TRANSPARENT_TEXT);
-
-	LoadLeftButton("BACK");
-	LoadMiddleButton("SET");
-
-	GrFlush(&g_sContext);
-}
-
-void PrintSettings_DateTime_NotAllowed(){
-	GrStringDrawCentered(&g_sContext, "Cannot edit", AUTO_STRING_LENGTH, 47, 27, OPAQUE_TEXT);
-	GrStringDrawCentered(&g_sContext, "while insulin", AUTO_STRING_LENGTH, 47, 38, OPAQUE_TEXT);
-	GrStringDrawCentered(&g_sContext, "delivery is", AUTO_STRING_LENGTH, 47, 49, OPAQUE_TEXT);
-	GrStringDrawCentered(&g_sContext, "in progress", AUTO_STRING_LENGTH, 47, 60, OPAQUE_TEXT);
-
-	LoadMiddleButton("OK");
-
-
-	GrFlush(&g_sContext);
-
 }
 
 void PrintCreateReminder_Idle(){
